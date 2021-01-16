72da01677cffd37670ec676f87fe2746714c58c1
Gustavo Kumasawa <gustavokumasawa@gmail.com>
2021-01-16 18:01:54 -0300

    Feat Rubik font from google fonts

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,6 +6,7 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     outline: 0;
+    font-family: 'Rubik', sans-serif;
   }
 
   body {
@@ -13,6 +14,8 @@ export default createGlobalStyle`
   }
   
   button {
+    border: none;
+    background: none;
     cursor: pointer;
   }
 `;
