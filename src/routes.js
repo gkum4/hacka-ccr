91d703dcbdccc5f4e1be130ab8df0ac4d4f63463
Gustavo Kumasawa <gustavokumasawa@gmail.com>
2021-01-16 17:18:41 -0300

    Feat routes

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/signup" component={SignUp} />
   </Switch>
 );
 
